420a6590f98d2f1cda9a200378718dde5dd69927
winashcs <ashwinarun.c@gmail.com>
2024-06-24 16:33:57 +0400

    Update README.md

--- ccms.py
+++ ccms.py
@@ -1,2 +1,3 @@
-from tkinter import*
+from tkinter import* # for creating graphic user interface
 from tkinter import ttk  # (themed tk) we use ttk for modern widgets like buttons, labels, and other GUI elements
+from PIL import Image,ImageTk # helps for image processing in gui
