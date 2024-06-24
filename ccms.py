5708b2cb202885366f03332e70cf8b22a136d972
winashcs <ashwinarun.c@gmail.com>
2024-06-24 16:51:22 +0400

    Update README.md

--- ccms.py
+++ ccms.py
@@ -1,3 +1,22 @@
 from tkinter import* # for creating graphic user interface
 from tkinter import ttk  # (themed tk) we use ttk for modern widgets like buttons, labels, and other GUI elements
 from PIL import Image,ImageTk # helps for image processing in gui
+
+class CC: #i used CC here because CyberCrime will be a long keyword
+    def __init__(self,root):
+        self.root=root
+        self.root.geometry('1300x700+0+0')
+        self.root.title('CYBER CRIME MANAGEMENT SYSTEM') 
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    root=Tk()
+    obj=CC(root)
+    root.mainloop()    
