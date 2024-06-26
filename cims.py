b7fca4e52908e183be16037d89e29d859ffde7da
winashcs <ashwinarun.c@gmail.com>
2024-06-26 14:37:14 +0400

    Update README.md

--- cims.py
+++ cims.py
@@ -1,8 +1,7 @@
 from tkinter import* # for creating graphic user interface
 from tkinter import ttk  # (themed tk) we use ttk for modern widgets like buttons, labels, and other GUI elements
 from PIL import Image,ImageTk # helps for image processing in gui
-from tkcalendar import Calendar  # module for calendar widget
-import datetime  # module for date operations
+from tkcalendar import Calendar  # module for calendar
 
 class CC: #i used CC here because CyberCrime will be a long keyword
     def __init__(self,root):
