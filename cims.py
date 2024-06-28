1c5ef50d9ff2a62c3cdb6e8e475b2768ebb709e4
winashcs <ashwinarun.c@gmail.com>
2024-06-28 11:57:29 +0400

    Update README.md

--- cims.py
+++ cims.py
@@ -62,23 +62,19 @@ class CC: #i used CC here because CyberCrime will be a long keyword
         self.i333=Label(frame1,image=self.i33)
         self.i333.place(x=910,y=0,width=455,height=130)
         
+        #creating various input fields for the user to enter        
         frame2=Frame(self.root,bd=2,relief=RIDGE,bg='white')
-        frame2.place(x=10,y=200,width=1336,height=495)
-        
+        frame2.place(x=10,y=200,width=1336,height=495)        
         frame2_1=LabelFrame(frame2,bd=2,relief=RIDGE,text='Cybersecurity Alerts',font=("Garamond",15,'bold'),fg='dark green',bg='white')
-        frame2_1.place(x=10,y=10,width=1316,height=238)
-        
+        frame2_1.place(x=10,y=10,width=1316,height=238)        
         b1=Label(frame2_1,text='Case ID :',font=("Ebrima",10,'bold'),fg='black',bg='white')
         b1.grid(row=0,column=0,padx=2,sticky=W,pady=2)
         b11=ttk.Entry(frame2_1,textvariable=self.var_case_id,width=20,font=("Times New Roman",10,'bold'))        
-        b11.grid(row=0,column=1,padx=2,sticky=W,pady=2)
-        
+        b11.grid(row=0,column=1,padx=2,sticky=W,pady=2)        
         b2=Label(frame2_1,text='Victim Name :',font=("Ebrima",10,'bold'),fg='black',bg='white')
         b2.grid(row=1,column=0,padx=2,sticky=W,pady=2)
         b22=ttk.Entry(frame2_1,textvariable=self.var_victim_name,width=20,font=("Times New Roman",10,'bold'))        
-        b22.grid(row=1,column=1,padx=2,sticky=W,pady=2)
-        
-        
+        b22.grid(row=1,column=1,padx=2,sticky=W,pady=2)      
         b3=Label(frame2_1,text='Victim Gender :',font=("Ebrima",10,'bold'),fg='black',bg='white')
         b3.grid(row=2,column=0,padx=2,sticky=W,pady=2)
         frame3_1=Frame(frame2_1,bd=1,relief=RIDGE,bg='white',highlightbackground="grey", highlightthickness=1)
@@ -86,13 +82,11 @@ class CC: #i used CC here because CyberCrime will be a long keyword
         b33_male=Radiobutton(frame3_1,variable=self.var_victim_gender,text='M',value='male',font=("Arial",10,'bold'),bg='white')
         b33_male.grid(row=0,column=0,pady=0,padx=10,sticky=W)
         b33_female=Radiobutton(frame3_1,variable=self.var_victim_gender,text='F',value='female',font=("Arial",10,'bold'),bg='white')
-        b33_female.grid(row=0,column=1,pady=0,padx=10,sticky=W)
-        
+        b33_female.grid(row=0,column=1,pady=0,padx=10,sticky=W)        
         b4=Label(frame2_1,text='Victim Details :',font=("Ebrima",10,'bold'),fg='black',bg='white')
         b4.grid(row=3,column=0,padx=2,sticky=W,pady=10)
         b44=ttk.Entry(frame2_1,textvariable=self.var_victim_details,width=20,font=("Times New Roman",10,'bold'))        
-        b44.grid(row=3,column=1,padx=2,sticky=W,pady=3)
-        
+        b44.grid(row=3,column=1,padx=2,sticky=W,pady=3)        
         b5=Label(frame2_1,text='Date of incident :',font=("Ebrima",10,'bold'),fg='black',bg='white')
         b5.grid(row=4,column=0,padx=2,sticky=W,pady=2)
         def show_calendar(event):
@@ -111,43 +105,35 @@ class CC: #i used CC here because CyberCrime will be a long keyword
             ok_button.pack(pady=10)        
         b55 = ttk.Entry(frame2_1,textvariable=self.var_date_of_incident,width=20, font=("Times New Roman", 10, 'bold'))
         b55.grid(row=4, column=1, padx=2, sticky=W, pady=2)
-        b55.bind('<Button-1>', show_calendar)
-        
+        b55.bind('<Button-1>', show_calendar)        
         b6=Label(frame2_1,text='Type of cybercrime :',font=("Ebrima",10,'bold'),fg='black',bg='white')
         b6.grid(row=0,column=2,padx=2,sticky=W,pady=2)
         b66=ttk.Entry(frame2_1,textvariable=self.var_type_of_cybercrime,width=20,font=("Times New Roman",10,'bold'))
-        b66.grid(row=0,column=3,padx=2,sticky=W,pady=2)
-        
+        b66.grid(row=0,column=3,padx=2,sticky=W,pady=2)        
         b7=Label(frame2_1,text='Type of cyberattack :',font=("Ebrima",10,'bold'),fg='black',bg='white')
         b7.grid(row=1,column=2,padx=2,sticky=W,pady=2)
         b77=ttk.Entry(frame2_1,textvariable=self.var_type_of_cyberattack,width=20,font=("Times New Roman",10,'bold'))
-        b77.grid(row=1,column=3,padx=2,sticky=W,pady=2)
-        
+        b77.grid(row=1,column=3,padx=2,sticky=W,pady=2)        
         b8=Label(frame2_1,text='Impact Assessment :',font=("Ebrima",10,'bold'),fg='black',bg='white')
         b8.grid(row=2,column=2,padx=2,sticky=W,pady=2)
         b88=ttk.Entry(frame2_1,textvariable=self.var_impact_assessment,width=20,font=("Times New Roman",10,'bold'))
-        b88.grid(row=2,column=3,padx=2,sticky=W,pady=2)
-        
+        b88.grid(row=2,column=3,padx=2,sticky=W,pady=2)        
         b9=Label(frame2_1,text='IP address :',font=("Ebrima",10,'bold'),fg='black',bg='white')
         b9.grid(row=3,column=2,padx=2,sticky=W,pady=2)
         b99=ttk.Entry(frame2_1,textvariable=self.var_ip_address,width=20,font=("Times New Roman",10,'bold'))
         b99.grid(row=3,column=3,padx=2,sticky=W,pady=2)
-
         b10=Label(frame2_1,text='Device Information :',font=("Ebrima",10,'bold'),fg='black',bg='white')
         b10.grid(row=4,column=2,padx=2,sticky=W,pady=2)
         b1010=ttk.Entry(frame2_1,textvariable=self.var_device_information,width=20,font=("Times New Roman",10,'bold'))
-        b1010.grid(row=4,column=3,padx=2,sticky=W,pady=2)
-    
+        b1010.grid(row=4,column=3,padx=2,sticky=W,pady=2)    
         b11=Label(frame2_1,text='Related Incident :',font=("Ebrima",10,'bold'),fg='black',bg='white')
         b11.grid(row=0,column=4,padx=2,sticky=W,pady=2)
         b1111=ttk.Entry(frame2_1,textvariable=self.var_related_incident,width=20,font=("Times New Roman",10,'bold'))
-        b1111.grid(row=0,column=5,padx=2,sticky=W,pady=2)
-        
+        b1111.grid(row=0,column=5,padx=2,sticky=W,pady=2)        
         b12=Label(frame2_1,text='Suspect Name :',font=("Ebrima",10,'bold'),fg='black',bg='white')
         b12.grid(row=1,column=4,padx=2,sticky=W,pady=2)
         b1212=ttk.Entry(frame2_1,textvariable=self.var_suspect_name,width=20,font=("Times New Roman",10,'bold'))
         b1212.grid(row=1,column=5,padx=2,sticky=W,pady=2)
-
         b13=Label(frame2_1,text='Suspect Gender :',font=("Ebrima",10,'bold'),fg='black',bg='white')
         b13.grid(row=2,column=4,padx=2,sticky=W,pady=2)
         frame3_2=Frame(frame2_1,bd=1,relief=RIDGE,bg='white',highlightbackground="grey", highlightthickness=1)
@@ -155,13 +141,11 @@ class CC: #i used CC here because CyberCrime will be a long keyword
         b1313_male=Radiobutton(frame3_2,variable=self.var_suspect_gender, text='M',value='Male',font=("Arial",10,'bold'),bg='white')
         b1313_male.grid(row=0,column=0,pady=0,padx=10,sticky=W)
         b1313_female=Radiobutton(frame3_2,variable=self.var_suspect_gender, text='F',value='Female',font=("Arial",10,'bold'),bg='white')
-        b1313_female.grid(row=0,column=1,pady=0,padx=10,sticky=W)              
-        
+        b1313_female.grid(row=0,column=1,pady=0,padx=10,sticky=W)           
         b14=Label(frame2_1,text='Suspect Details :',font=("Ebrima",10,'bold'),fg='black',bg='white')
         b14.grid(row=3,column=4,padx=2,sticky=W,pady=2)
         b1414=ttk.Entry(frame2_1,textvariable=self.var_suspect_details,width=20,font=("Times New Roman",10,'bold'))
-        b1414.grid(row=3,column=5,padx=2,sticky=W,pady=2)
-        
+        b1414.grid(row=3,column=5,padx=2,sticky=W,pady=2)        
         b15=Label(frame2_1,text='Status :',font=("Ebrima",10,'bold'),fg='black',bg='white')
         b15.grid(row=4,column=4,padx=2,sticky=W,pady=2)
         self.var_status = StringVar(frame2_1)
